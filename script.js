20b8b01f0fe7369c3f84528e0aeda65efb67d739
gohar-anjum <goharanjum49@gmail.com>
2024-07-24 18:55:09 +0500

    PopUp Menu Done

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.querySelector("#icony").addEventListener("click", function(val){
+    document.querySelector("#menuitems").style.display="initial"
+})
+//cross button
+document.querySelector("#cancelbtn").addEventListener("click", function(val){
+    document.querySelector("#menuitems").style.display="none"
+})
